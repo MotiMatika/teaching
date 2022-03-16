2b57d67ac9a30c416cad1190cad747f7393e40a5
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-03-16 23:34:44 +0200

    Add files via upload

---
+++ Lesson #7 if_1.py
@@ -0,0 +1,72 @@
+# if 2<5:
+#     print("yonatan")
+
+
+# if 2>5:
+#      print("yonatan")
+
+
+# if 2>5:
+#     print("yonatan")
+# else:
+#     print("moti")
+
+
+# if a>5:
+#      print("yonatan")
+# else:
+#     print("moti")
+
+
+# a=7
+# if a>5:
+#      print("yonatan")
+# else:
+#     print("moti")
+
+
+# if a>b:
+#      print("yonatan")
+# else:
+#     print("moti")
+
+
+# if "a">"b":
+#      print("yonatan")
+# else:
+#     print("moti")
+
+
+# if "a"<"b":
+#      print("yonatan")
+# else:
+#     print("moti")
+
+
+# a=5
+# b=3
+# if a>b:
+#      print("yonatan")
+# else:
+#     print("moti")
+
+
+# a=5
+# b=3
+# if a+b>b:
+#      print("yonatan")
+# else:
+#     print("moti")
+
+
+a=5
+b=3
+if a*b==b*a:
+     print("yonatan")
+else:
+    print("moti")
+
+
+
+
+
