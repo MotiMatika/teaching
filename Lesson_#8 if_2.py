082778d1ace168baf941bedb0402b88f34966a94
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-03-17 17:46:32 +0200

    Add files via upload

---
+++ Lesson_#8 if_2.py
@@ -0,0 +1,115 @@
+#ממיינת בין 2 מספרים מי גדול יותר רק אם הם שונים
+# x = int(input ("a = "))
+# y = int(input ("b = "))
+# if x>y:
+#     print(x,">",y)
+# else:
+#     print(y,">",x)
+
+
+#ממיינת בין 2 מספרים מי גדול יותר גם אם הם שווים
+
+# a = int(input ("a = "))
+# b = int(input ("b = "))
+# if a>b:
+#     print(a,">",b)
+# elif a==b:
+#     print(a,"=",b)
+# else:
+#     print(b,">",a)
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# משווה יחסית למספר קבוע
+# num = int(input(" ?בחר מספר.מהי הבחירה שלך"))      #קלט של מספר מהמשתמש
+# x=10                                        # השמת ערך בתא איקס
+# if num > x:                                 #אם המספר שהקליד המשתמש גדול מ 10 ... אז
+# 	print(num,">",x)                        # פלט-המספר שבחרת גדול מ 10
+# elif num < x:                               #אחרת- אם המספר שהקליד המשתמש קטן מ 10 ... אז
+# 	print(num,"<",x)                        #פלט-המספר שבחרת קטן מ 10
+# else:                                       # אחרת
+# 	print(num,"=",x)
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# # משווה יחסית למספר קבוע
+# print("Choose a Number ",)                        # פלט - הודעה
+# choice1 = int(input("What is Your Choice?  "))    #קלט של מספר מהמשתמש
+# x=10                                              # השמת ערך בתא איקס
+# if choice1 > x:                                   #אם המספר שהקליד המשתמש גדול מ 10 ... אז
+# 	print("your number is bigger than ",x)         # פלט-המספר שבחרת גדול מ 10
+# elif choice1 < x:                                 #אחרת- אם המספר שהקליד המשתמש קטן מ 10 ... אז
+# 	print("your number is smaller than ",x)        #פלט-המספר שבחרת קטן מ 10
+# else:                                             # אחרת
+# 	print("your number is equal to ",x) 
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#התכנית תמיין שלשה מספרים רק אם הם שונים
+# a = input ("a = ")
+# b = input ("b = ")
+# c = input ("c = ")
+# if a>b:
+#     if a>c :
+#         if b>c:
+#             print(" "*5,"a > b > c\n"," "*4,a,">",b,">",c)
+#         else:
+#             print(" "*5,"a > c > b\n"," "*4,a,">",c,">",b)
+#     else:
+#         print(" "*5,"c > a > b\n"," "*4,c,">",a,">",b)
+# else:
+#     if b>c:
+#         if a>c:
+#            print(" "*5,"b > a > c\n"," "*4,b,">",a,">",c) 
+#         else:
+#             print(" "*5,"b > c > a\n"," "*4,b,">",c,">",a)
+#     else:
+#         print(" "*5,"c > b > a\n"," "*4,c,">",b,">",a)        
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#שידרוג ?
+# התכנית תמיין שלשה מספרים רק אם הם שונים
+# a = input ("a = ")
+# b = input ("b = ")
+# c = input ("c = ")
+# if a>b and a>c and b>c:
+#     print(" "*5,"a > b > c\n"," "*4,a,">",b,">",c)
+# if a>b and a>c and b<c:
+#     print(" "*5,"a > c > b\n"," "*4,a,">",c,">",b)
+# if a>b and a<c :
+#     print(" "*5,"c > a > b\n"," "*4,c,">",a,">",b)
+# if b>a and b>c and a>c:
+#     print(" "*5,"b > a > c\n"," "*4,b,">",a,">",c)
+# if b>c and a<c :
+#         print(" "*5,"b > c > a\n"," "*4,b,">",c,">",a)
+# if b>a and b<c:
+#       print(" "*5,"c > b > a\n"," "*4,c,">",b,">",a)  
+ 
+
+
+# # התכנית תבדוק אם שלשה מספרים שווים זה לזה
+# a = input ("a = ")
+# b = input ("b = ")
+# c = input ("c = ")
+# if a==b and a==c:
+#     print("\n     equal")
+# else:
+#     print("\n     not equal")
+
+#והתכנית תבדוק אם שלש התוצאות שוות זו לזה
+# a = 4*256
+# b = 8*128
+# c = 16*64
+# d = 32*32
+# e = 2*512
+# if a==b and a==c and a==d and a==e:
+#     print("\n     תווש equal")
+# else:
+#     print("\n     תווש אל not equal")
+
+
+#בודקת אם תו/אות נמצא בתוך מחרוזת
+# name = "moti"
+# if "j" in name:
+#     print("good")
+# else:
+#     print("not good")
---
+++ Lesson_#9 if_3.py
@@ -0,0 +1,29 @@
+# temp = 19
+# is_raining = False
+
+# if temp > 15:
+#     print("take a hat")
+#     print("don't take a coat")
+# else:
+#     print("don't take a hat")
+#     print("take a coat")
+# if is_raining==True:
+#     print("take an umbrella")
+# else:
+#     print("don't take an umbrella")
+
+
+temp = int(input("\nwhat is the tempreture ? "))
+is_raining = input("\nis it raining ? ")
+
+if temp > 15:
+    print("\ntake a hat")
+    print("don't take a coat")
+else:
+    print("\ndon't take a hat")
+    print("take a coat")
+
+if is_raining=="yes":
+    print("take an umbrella")
+else:
+    print("don't take an umbrella")
