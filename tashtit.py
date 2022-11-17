12b041927de9e66be156c6c4ea814c6e64a77538
MotiMatika <motimatika@gmail.com>
2022-11-17 18:24:13 +0200

    5

--- tashtit.py
+++ tashtit.py
@@ -1,4 +1,8 @@
-#3.5.2
-txt=input("please enter a word : ")                                  #קלט של מחרוזת
-count_num = len(txt)                                                 #אורך המחרוזת
-print("_ " * count_num)
+# #3.5.2
+# txt=input("please enter a word : ")                                  #קלט של מחרוזת
+# count_num = len(txt)                                                 #אורך המחרוזת
+# print("_ " * count_num)
+
+import random
+a=random.randint(1,12)
+print (a)
